400f71c350165911f75c7cb429978dc0ba5769e9
Leonardocasilva <lcsilva80@yahoo.com.br>
2018-09-03 01:38:15 -0300

    Adicionado tela de load, para visualizar melhor o processo de assync funcionando

---
+++ AsyncWait/Loading.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncWait
+{
+    public partial class Loading : Form
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+
+        private void Loading_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
